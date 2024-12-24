19ec4e97fd766cd3084a352d88abdf41e827686f
Igor Chorazewicz <igor.chorazewicz@intel.com>
2024-12-24 01:48:13 +0100

    e2e l0 v2 fixes

--- sycl/test-e2e/ESIMD/unified_memory_api/atomic_update_acc_dg2_pvc_stateless.cpp
+++ sycl/test-e2e/ESIMD/unified_memory_api/atomic_update_acc_dg2_pvc_stateless.cpp
@@ -5,10 +5,3 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //=-----------------------------------------------------------------------=//
-
-// REQUIRES: arch-intel_gpu_pvc || gpu-intel-dg2
-
-// RUN: %{build} -fsycl-esimd-force-stateless-mem -o %t.out
-// RUN: %{run} %t.out
-
-#include "atomic_update_acc_dg2_pvc.cpp"
