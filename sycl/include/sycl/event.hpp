02209cdcd6a0135add8573abe9bb8fe2b911bb49
Igor Chorazewicz <igor.chorazewicz@intel.com>
2025-04-02 01:25:40 +0000

    XXX

--- sycl/include/sycl/event.hpp
+++ sycl/include/sycl/event.hpp
@@ -147,6 +147,8 @@ public:
   /// \return the backend associated with this platform
   backend get_backend() const noexcept;
 
+  ur_event_handle_t urHandle = nullptr;
+
 private:
   event(std::shared_ptr<detail::event_impl> EventImpl);
 
